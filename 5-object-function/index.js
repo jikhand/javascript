54f316104c36f8e857f783243fb113906569d90b
khandajitesh <39655291+khandajitesh@users.noreply.github.com>
2020-07-05 11:17:41 +0530

    1st commit

---
+++ 5-object-function/index.js
@@ -0,0 +1,12 @@
+var v = {
+    at:'Gududa',
+    name:'Jitesh',
+    age:23
+}
+var Address = function(at,name,age){
+    this.at='Gududa',
+    this.name='katio',
+    this.age=29
+}
+
+console.log(Address.name);
